d61fa0561f400255f6db10b07bee7d4ad86644d1
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2023-07-30 02:42:45 +0900

    Graphics系Nodeの破棄時にリソースを開放するように修正

--- src/node/GraphicsNode.h
+++ src/node/GraphicsNode.h
@@ -1,7 +1,7 @@
 /**********
  * Author:  Y.Nakaue
  * Created: 2023/07/21
- * Edited:  2023/07/21
+ * Edited:  2023/07/30
  **********/
 
 #ifndef __GRAPHICS_NODE_H__
@@ -17,6 +17,15 @@ class GraphicsNode : public Node
 {
 public:
     virtual cv::Mat getContent(Pin::Type pin_type) = 0;
+
+    virtual ~GraphicsNode()
+    {
+        /////////////////////////////
+        ///// Release resources /////
+        /////////////////////////////
+        this->m_image.release();
+        glDeleteTextures(1, &this->m_gl_texture);
+    }
 
     int32_t m_width  = 100;
     int32_t m_height = 100;
